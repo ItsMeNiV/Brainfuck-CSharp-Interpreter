07b73ad87100a505fd6323ce0c0dd0f69eb67e17
Yannik Hodel <yannik.hodel@gmail.com>
2014-06-27 11:19:49 +0200

    Works with good code. Didn't test it and still need to add Error-Handling

---
+++ BFInterpreter/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace BFInterpreter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_code = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txt_input = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txt_output = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_code);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(477, 49);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Code";
+            // 
+            // txt_code
+            // 
+            this.txt_code.Location = new System.Drawing.Point(7, 19);
+            this.txt_code.Name = "txt_code";
+            this.txt_code.Size = new System.Drawing.Size(464, 20);
+            this.txt_code.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txt_input);
+            this.groupBox2.Location = new System.Drawing.Point(12, 67);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(477, 49);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Input";
+            // 
+            // txt_input
+            // 
+            this.txt_input.Location = new System.Drawing.Point(7, 19);
+            this.txt_input.Name = "txt_input";
+            this.txt_input.Size = new System.Drawing.Size(464, 20);
+            this.txt_input.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(414, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Execute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txt_output);
+            this.groupBox3.Location = new System.Drawing.Point(19, 151);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(470, 99);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Output";
+            // 
+            // txt_output
+            // 
+            this.txt_output.Location = new System.Drawing.Point(7, 20);
+            this.txt_output.Name = "txt_output";
+            this.txt_output.ReadOnly = true;
+            this.txt_output.Size = new System.Drawing.Size(457, 73);
+            this.txt_output.TabIndex = 0;
+            this.txt_output.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 262);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_code;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txt_input;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox txt_output;
+    }
+}
+
---
+++ BFInterpreter/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BFInterpreter
+{
+    public partial class Form1 : Form
+    {
+
+        char[] mainAr = new char[900];
+        char[] code = new char[900];
+        int[] input = new int[900];
+
+        private string text = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start(String code, String input)
+        {
+            clearMainAr();
+            getCode(code);
+            getInput(input);
+            translate();
+            writeText();
+        }
+
+        private void translate()
+        {
+            bool loopOpen = false;
+            int position = 0;
+            int z = 0;
+            //Main-Loop
+            for (int i = 0; i < code.Length; i++)
+            {
+                switch (code[i])
+                {
+                    case '<':
+                        if (position > 0)
+                        {
+                            position--;
+                        }
+                        else
+                        {
+                            //ERROR
+                        }
+                        break;
+                    case '>':
+                        if (position < 900)
+                        {
+                            position++;
+                        }
+                        else
+                        {
+                            //ERROR
+                        }
+                        break;
+                    case '+':
+                        mainAr[position]++;
+                        break;
+                    case '-':
+                        if (mainAr[position] > 0)
+                        {
+                            mainAr[position]--;
+                        }
+                        else
+                        {
+                            //ERROR
+                        }
+                        break;
+                    case ',':
+                        mainAr[position] = (char)input[z];
+                        z++;
+                        break;
+                    case '.':
+                        if (mainAr[position] > (int)31)
+                        {
+                            text += mainAr[position].ToString();
+                        }
+                        else
+                        {
+                            int a = (int)mainAr[position];
+                            text += a;
+                        }
+                        break;
+                    case '[':
+                        if (mainAr[position] == 0)
+                        {
+                            //Jump to closing brackets if value of current field is 0. If brackets inside brackets jump over these too.
+                            i++;
+                            int y = 0;
+                            for (int x = i; x < code.Length; x++)
+                            {
+                                if (code[x] == '[')
+                                {
+                                    y++;
+                                }
+                                else if (code[x] == ']' && y > 0)
+                                {
+                                    y--;
+                                }
+                                else if (code[x] == ']' && y == 0)
+                                {
+                                    i = x;
+                                    break;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            loopOpen = true;
+                        }
+                        break;
+                    case ']':
+                        if (!loopOpen)
+                        {
+                            //ERROR
+                        }
+                        else if (loopOpen && mainAr[position] != 0)
+                        {
+                            int y = 0;
+                            int x = i;
+                            while (x != 0)
+                            {
+                                x--;
+                                if (code[x] == ']')
+                                {
+                                    y++;
+                                }else if(code[x] == '[' && y > 0){
+                                    y--;
+                                }
+                                else if (code[x] == '[' && y == 0)
+                                {
+                                    i = x;
+                                    break;
+                                }
+                            }
+                        }
+                        else if (loopOpen && mainAr[position] == 0)
+                        {
+                            loopOpen = false;
+                        }
+                        break;
+                }
+            }
+        }
+
+        private void getInput(string input2)
+        {
+            if (input2 != "")
+            {
+                char[] in2 = input2.ToCharArray();
+                for (int i = 0; i < in2.Length; i++)
+                {
+                    input[i] = (int)Char.GetNumericValue(in2[i]);
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void clearMainAr()
+        {
+            for (int i = 0; i < mainAr.Length; i++)
+            {
+                mainAr[i] = (char)0;
+            }
+            text = "";
+        }
+
+        private void getCode(string code2)
+        {
+            code = code2.ToCharArray();
+        }
+
+        private void writeText()
+        {
+            txt_output.Text = text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txt_output.Clear();
+            start(txt_code.Text, txt_input.Text);
+        }
+
+    }
+}
